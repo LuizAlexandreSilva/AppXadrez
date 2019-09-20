53f53f7b8d02753569eeef3a83b7f569fd658653
Luiz Alexandre <alexandrehsilva96@gmail.com>
2019-09-20 09:51:08 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const logger = require("morgan");
+const router = require('./config/routes');
+const app = express();
+const handlebars = require('express-handlebars');
+const sass = require('node-sass-middleware');
+
+const PORT = 3000;
+
+app.use(logger("short"));
+
+app.use('/img', [
+    express.static(__dirname + '/public/img')
+]);
+
+app.use(router);
+
+app.use(sass({
+    src: __dirname + '/public/scss',
+    dest: __dirname + '/public/css',
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css'
+}));
+
+app.use('/js', [
+    express.static(__dirname + '/node_modules/jquery/dist/'),
+    express.static(__dirname + '/node_modules/popper.js/dist/umd/'),
+    express.static(__dirname + '/node_modules/bootstrap/dist/js/'),
+    express.static(__dirname + '/public/js'),
+]);
+
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/app/views');
+
+app.use(function(req, res) {
+    res.statusCode = 404;
+    res.send("Página inexistente.");
+});
+
+app.listen(PORT, function() {
+    console.log("Escutando na porta " + PORT);
+});
---
+++ app/controllers/main.js
@@ -0,0 +1,17 @@
+const index = (req, res) => {
+    const conteudo = 'Página principal da aplicação';
+    res.render('main/index', {
+        conteudo: conteudo,
+        layout: false
+    });
+}
+
+const sobre = (req, res) => {
+    const conteudo = 'Página sobre a aplicação';
+    res.render('main/sobre', {
+        conteudo: conteudo,
+        layout: false
+    });
+}
+
+module.exports = { index, sobre }
